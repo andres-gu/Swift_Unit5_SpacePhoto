c7cd0d0736843bbbe6ff50b0d8294fbf9be5e603
Andres G <andres_gu@icloud.com>
2018-01-30 11:28:45 -0700

    Pre- grand central dispatch (GCD)

---
+++ PhotoInfoController.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoInfoController.swift
+//  SpacePhoto
+//
+//  Created by Andres Gutierrez on 1/30/18.
+//  Copyright © 2018 Andres Gutierrez. All rights reserved.
+//
+
+import Foundation
+
+
+
+func fetchPhotoInfo(completion: @escaping (PhotoInfo?) -> Void) {
+    
+    let baseURL = URL(string: "https://api.nasa.gov/planetary/apod")!
+    
+    let query: [String: String] = [
+        "api_key": "DEMO_KEY",
+        "date": "2018-01-29"
+    ]
+    
+    let url = baseURL.withQueries(query)!
+    
+    let task = URLSession.shared.dataTask(with: url) { (data, response, err) in
+        let jsonDecoder = JSONDecoder()
+        print("data:: ", data)
+        if let data = data, let photoInfo = try? jsonDecoder.decode(PhotoInfo.self, from: data) {
+            completion(photoInfo)
+        } else {
+            print("No data was found, or data was not properly decoded.")
+            completion(nil)
+        }
+    }
+    task.resume()
+}
+
---
+++ ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  SpacePhoto
+//
+//  Created by Andres Gutierrez on 1/30/18.
+//  Copyright © 2018 Andres Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var apodImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var copyrightLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        descriptionLabel.text = ""
+        copyrightLabel.text = ""
+        
+        fetchPhotoInfo { (photoInfo) in
+            if let photoInfo = photoInfo {
+                self.title = photoInfo.title
+                self.descriptionLabel.text = photoInfo.description
+            }
+            if let copyright = photoInfo?.copyright {
+                self.copyrightLabel.text = "Copyright \(copyright)"
+            } else {
+                self.copyrightLabel.text = "v"
+            }
+        }
+        
+    }
+    
+    
+    
+}
